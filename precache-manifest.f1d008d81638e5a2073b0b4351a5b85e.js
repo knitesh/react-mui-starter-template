925bdf0f58a5cf0c2695c3e9e5e468f00c0e3502
Kumar Nitesh <knitesh@smartdestinations.com>
2019-04-08 14:36:47 -0400

    Updates

--- precache-manifest.f1d008d81638e5a2073b0b4351a5b85e.js
+++ precache-manifest.f1d008d81638e5a2073b0b4351a5b85e.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/react-mui-starter-template/static/js/runtime~main.d47801cf.js"
   },
   {
-    "revision": "a2bda2417cd1e2e80726",
-    "url": "/react-mui-starter-template/static/js/main.d66eaa1b.chunk.js"
+    "revision": "df0f97575541de6a9462",
+    "url": "/react-mui-starter-template/static/js/main.09b32839.chunk.js"
   },
   {
     "revision": "5b471dd1d0c3cc12ab01",
     "url": "/react-mui-starter-template/static/js/2.e1b0b555.chunk.js"
   },
   {
-    "revision": "a2bda2417cd1e2e80726",
+    "revision": "df0f97575541de6a9462",
     "url": "/react-mui-starter-template/static/css/main.d5e85bf2.chunk.css"
   },
   {
-    "revision": "fb2bea8e6d2aadced65674571c76cac9",
+    "revision": "12f9d477e1c01b2d4318c31fdcd8218b",
     "url": "/react-mui-starter-template/index.html"
   }
 ];
